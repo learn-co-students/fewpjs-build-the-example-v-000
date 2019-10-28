1a3064473030ad5dd1b5bea1b868c4a5b4cb40cb
AlwinaO <aoyewole2586@gmail.com>
2019-10-28 16:00:31 -0400

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,37 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const hearts = document.querySelectorAll(".like");
+document.addEventListener("DOMContentLoaded", function(){
+  return hearts.forEach((heart) => {
+    heart.addEventListener("click", likeCallback)
+  })
+})
 
+console.log(hearts);
+
+const heart = (event) => {
+  event.preventDefault();
+  return fetch(event)
+    .then((response) => response.text)
+    .then((heart) => {
+      document.querySelector(".like-glyph") = heart;
+    })
+    .catch((error) => {
+      document.querySelector("#modal.hidden") = error;
+      error.removeAttribute(".hidden");
+      document.getElementById("modal-message") = error.message;
+      setTimeout(function to be executed, 5000)
+    })
+}
+
+// create a variable called heart = event
+// .then and then .catch
+
+const likeCallback = (event) => {
+  // console.log(event);
+  mimicServerCall();
+}
 
 
 //------------------------------------------------------------------------------
