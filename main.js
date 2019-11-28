dc2849340d39afb5a35fb92dd7e08e7591c179ba
Gingertonic <bethmschofield@gmail.com>
2019-11-28 19:26:15 +0000

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,44 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errModal = document.querySelector('#modal')
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', setup)
 
+function setup(){
+  const hearts = document.querySelectorAll('.like-glyph')
+  
 
+  for (const heart of hearts) { heart.addEventListener('click', toggleLike)}
+}
+
+function toggleLike(e){
+  mimicServerCall()
+    .then(updateUI(e))
+    .catch(handleError)  
+}
+
+function updateUI(e){
+  console.log(e.target.classList)
+  if (e.target.classList.contains('activated-heart')){
+    e.target.classList.remove('activated-heart')
+    e.target.textContent = "♡"
+  } else {
+    e.target.classList.add('activated-heart')
+    e.target.textContent = '❦'
+  }
+}
+
+function handleError(err){
+  errModal.classList.remove('hidden')
+  errModal.textContent = `Oops! ${err}`
+  setTimeout(hideModal, 5000)
+}
+
+function hideModal(){
+  errModal.classList.add('hidden')
+}
 
 
 //------------------------------------------------------------------------------
