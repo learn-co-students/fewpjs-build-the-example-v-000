d3997c16e5c7de41e6aaafc05fd65ef3cb01f08e
ElvinGarcia <dimix181@gmail.com>
2020-01-19 16:51:33 -0500

    all test passing

--- main.js
+++ main.js
@@ -1,8 +1,51 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
+
+
+
+document.addEventListener("DOMContentLoaded", () => { HideErrorBanner(); eventListeners(); });
+function eventListeners() {
+  const like = document.getElementsByClassName("like-glyph");
+  for (const e of like) { e.addEventListener("click", (e) => { liker(e) }) }
+}
+
+function HideErrorBanner(){
+  const modal = document.getElementById("modal")
+  modal.setAttribute("class", "hidden");
+}
+
+function displayErrorBanner(e){
+  const modal = document.getElementById("modal")
+  modal.removeAttribute("class", "hidden");
+  
+  const modal_message = document.getElementById("modal-message")
+  setTimeout(() => { HideErrorBanner()},5000)
+  modal_message.innerText = e ;
+  console.log(e)
+}
+    
+function liker(e) {
+  mimicServerCall().then((result) => { 
+    HideErrorBanner();
+    if(e.target.innerText === EMPTY_HEART){ 
+      e.target.innerText = FULL_HEART;
+      e.target.setAttribute("class","activated-heart");
+    }else{
+      e.target.innerText = EMPTY_HEART;
+      e.target.removeAttribute("class","activated-heart");
+    }
+    console.log(result)
+  }).catch((error) => { displayErrorBanner(error)})
+ 
+
+} 
+
+
+
+
+
 
 
 
