21cae82c81f82716b1eea97ec6d438b9a92da2dc
Dana ONeil <doneil1580@gmail.com>
2020-02-15 18:50:12 -0500

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//  hide the modal upon screen load
+let modal = document.getElementById('modal')
+modal.className = 'hidden'
+
+let likes = document.getElementsByClassName('like-glyph')
+
+Array.from(likes).forEach(element => {
+  element.addEventListener("click", function(){
+    mimicServerCall()
+    .then()
+    .catch((error) => {
+      modal.className = ""
+      modal.innerHTML = error 
+      modalTimeOut()
+    })
+    .then(heartStatus(element))
+  })
+})
+
+function modalTimeOut(){
+  setTimeout(function(){
+    modal.className = 'hidden'}, 3000
+  )
+}
+
+function heartStatus(element) {
+  if(element.innerTEXT === EMPTY_HEART || element.innerTEXT === '&#x2661;'){
+    element.innerTEXT = FULL_HEART
+    element.className = 'activated-heart'
+  } else {
+    element.innerTEXT = EMPTY_HEART
+    element.className = ""
+  }
+}
 
 
 
@@ -23,3 +57,8 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+document.getElementById().addEventListener("click", 
+  function(){
+    document.getElementById("").innerHTML = "";
+  })
