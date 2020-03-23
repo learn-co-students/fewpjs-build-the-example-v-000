8397217201b28c4bb42d8711a47b1283e91f5d12
Blake A. Long <blake.a.long@gmail.com>
2020-03-23 16:08:33 -0600

    Done.

--- main.js
+++ main.js
@@ -2,10 +2,38 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+mimicServerCall()
+.then(response => {
+  addListeners()
+})
+.catch(error => {
+  alert("something happened on mimicServerCall")
+  errorModal()
+})
 
+const addListeners = () => {
+  likesArray = Array.from(document.getElementsByClassName('like-glyph'))
+  likesArray.forEach(element => {
+    element.addEventListener('click', () => {
+      if (element.innerText === EMPTY_HEART) {
+        element.innerText = FULL_HEART
+        element.className = 'activated-heart'
+      } else {
+        element.innerText = EMPTY_HEART
+        element.className = ''
+      }
+    })
+  })
+}
 
-
+const errorModal = () => {
+  let modal = document.getElementById('modal') 
+  modal.className = ''
+  const hide = () => {modal.className = 'hidden'}
+  setTimeout(hide, 5000)
+}
+// const addListeners = (elements) => {elements.map(element => element.addEventListener('click', console.log('mapped it ehre!!!')))}
+// addListeners(likesArray)
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
