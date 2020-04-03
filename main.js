1d00ed0f2e13f96f46b18dc4de8c8839dd558a7c
Kat Tochowicz <ktochow1@binghamton.edu>
2020-04-03 15:15:42 -0400

    Done.

--- main.js
+++ main.js
@@ -1,10 +1,51 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
-
+// const EMPTY_HEART = '♡'
+// const FULL_HEART = '♥'
+//
 // Your JavaScript code goes here!
+//
 
 
+  //
+  // let data = {
+  //   name: "Kat",
+  //   age: "24"
+  // };
+  // let configObj = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Accept": "application/json"
+  //   },
+  //   body: JSON.stringify(data)
+  // };
+  // return fetch('file:///Users/Katrina/Development/fewpjs-build-the-example-v-000/index.html', configObj)
+  // .then(function(response){
+  //   return response.json();
+  // }).then(function(obj){
+  //   console.log(obj);
+  // }).catch(function(error){
+  //   alert("Oh-uh");
+  //   console.log(error.message);
+  // });
+// })
+
+let liHearts = document.querySelectorAll(".like");
+document.addEventListener("DOMContentLoaded", () => {
+  const EMPTY_HEART = '♡'
+  const FULL_HEART = '♥'
+  let colorStates = {
+    "red" : "",
+    "": "red"
+  };
+
+  let likes = document.getElementsByClassName("likes");
+  let li = document.getElementsByTagName("li");
+  let error = document.getElementById("modal");
+  error.hidden = true;
+  mimicServerCall("fakenews");
+
+})
 
 
 //------------------------------------------------------------------------------
