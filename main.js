38a6795b34cc1a1390a6c79c73b949cdd477cf93
azharshahstl <azharshahstl@yahoo.com>
2020-05-21 13:50:34 -0500

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-// Defining text characters for the empty and full hearts for you to use later.
+ // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("click", function(e) {
+    const heart = e.target;
+    mimicServerCall()
+    .then(function(servresp){
+      if (heart.innerHTML === EMPTY_HEART){
+        heart.className = "activated-heart";
+        heart.innerHTML = FULL_HEART;
+      } else {
+        heart.className = "like-glyph";
+        heart.innerHTML = EMPTY_HEART;
+      }
+    })
+    .catch(function(error){
+      let hideClass = document.getElementsByClassName("hidden");
+      hideClass[0].classList.remove("hidden");
+      let modalMessage = document.getElementById("modal-message");
+      modalMessage.innerHTML = error;
+      setTimeout(function(){ 
+        let newClassName = document.getElementById("modal");
+        newClassName.className = "hidden" }, 5000);
+    })
 
+  })
+})
 
 
 
