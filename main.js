78e54f848fdac12f89f8e32bf70d47afb6e01a9a
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-28 10:29:44 -0500

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  const hearts = Array.from(document.getElementsByClassName('like-glyph'));
+  hearts.forEach(heart => {
+    heart.addEventListener("click", function(event) {
+      mimicServerCall()
+      .then(function(response) {
+        if (heart.innerHTML === EMPTY_HEART) {
+          heart.className = "activated-heart";
+          heart.innerHTML = FULL_HEART;
+        } else {
+          heart.className = "like-glyph";
+          heart.innerHTML = EMPTY_HEART;
+        }
+      })
+      .catch(function(error) {
+        let modal = document.getElementById('modal')
+        modal.className = "";
+        let modalMessage = document.getElementById('modal-message');
+        modalMessage.innerHTML = error;
+        setTimeout(function() {
+          modal.className = "hidden" }, 5000);
+      })
+    })
+  })
+})
+
 
 
 
