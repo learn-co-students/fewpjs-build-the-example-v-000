718833c088d84049ec4f417583af17717a6e7286
Mitchell Beebe <beebe.mitch@gmail.com>
2020-08-18 21:13:35 -0400

    Done.

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
+const switcher = {'♡': '♥', '♥':'♡'};
+const hearts = document.querySelectorAll(".like-glyph");
+const modal = document.getElementById("modal");
 
 // Your JavaScript code goes here!
-
-
-
+document.addEventListener('DOMContentLoaded', function() {
+  hearts.forEach(heart => {
+    heart.addEventListener("click", (e) => {
+      e.preventDefault()
+      mimicServerCall()
+        .then(() => {
+          if (heart.innerHTML === EMPTY_HEART) {
+            heart.setAttribute("class", "activated-heart");
+          } else {
+            heart.setAttribute("class", "like-glyph");
+          }
+          heart.innerHTML = switcher[heart.innerHTML];
+        })
+        .catch(() => {
+          modal.removeAttribute("class");
+          setTimeout(function() { 
+            modal.setAttribute("class", "hidden"); 
+          }, 3000);
+        })
+    })
+  })
+});
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
