ebf86606d4705f74f4dd4ece25a613c9f79cac7b
Eric Johnson <ericjohnson0216@icloud.com>
2020-09-10 22:57:25 -0400

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+var hash = {
+  EMPTY_HEART: FULL_HEART,
+  FULL_HEART: EMPTY_HEART
+}
+var modal = document.querySelector(".modal")
+var liker = document.querySelector(".like")
 // Your JavaScript code goes here!
 
+liker.addEventListener("click", (event) => {
+  mimicServerCall(function (url) { 
+  .then((result => event.target.innerText = hash[event.target.innerText]
+  .catch((error => {
+    modal.classList.remove("hidden")
+    })
+  })
+});
 
 
 
