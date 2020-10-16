5afbc30fc147c3259319d35cb52a5e6cdf4809c0
eryzna <eryzna@gmail.com>
2020-10-16 13:39:36 -0700

    Done.

--- main.js
+++ main.js
@@ -2,7 +2,46 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+let articleHearts = document.querySelectorAll(".like-glyph");
+
+function likeCallback(e) {
+  let heart = e.target;
+  mimicServerCall()
+    .then(function(serverMessage){
+      // STEP 2: Uncomment the next 3 lines.
+      // We'll use Pillar 1 (DOM Manipulation) to update the screen and
+      // mimic Pillar 3 (Server Communication) to only update the screen if the
+      // sending of information to the server succeeds.
+      alert("You notified the server!");
+      alert(serverMessage);
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      //alert("Something went wrong!");
+      document.getElementById("modal").className = "";
+    });
+}
+
+// STEP 3: In order for the call to the server and the update of the screen to
+// work, the elements we identify in STEP 1 need to be told to run that update
+// code when an "event" is fired. That's Pillar 2, event handling. Uncomment
+// this code.
+
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
+
 
 
 
